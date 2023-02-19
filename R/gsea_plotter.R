9f85bebb75193cc764430b672333f5b1fc78918a
Atakan Ekiz <38366155+atakanekiz@users.noreply.github.com>
2023-02-19 12:02:42 +0300

    updated minSize and maxSize to fgsea defaults

--- R/gsea_plotter.R
+++ R/gsea_plotter.R
@@ -77,7 +77,7 @@
 
 
 gsea_plotter <- function(exprs = NULL, preranked_genes = NULL, pos_marker = NULL, neg_marker = NULL, sample_id = NULL, sample_cluster = NULL,
-    reference_id = NULL, reference_cluster = NULL, method = "s2n", gene_set = msigdb_hallmark, minSize = 50, maxSize = 500, top_n = 10,
+    reference_id = NULL, reference_cluster = NULL, method = "s2n", gene_set = msigdb_hallmark, minSize = 1, maxSize = ncol(exprs)-3, top_n = 10,
     gseaParam = 1, plot_individual = NULL, append_title = F, top_plots_title = T, seed = 123, keep_results = T, save_png = F, png_units = "in",
     png_width = 4, png_height = 3, append_to_filename = "", verbose = T, annot_text_color = "black", annot_text_size = 4, annot_text_fontface = 2, ...) {
 
